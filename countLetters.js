b171ac0c39503858ed1a560c49d363b386cb53ae
Dylan Leard <dleard@uvic.ca>
2019-01-09 21:14:25 +0000

    fixed for counting spaces

--- countLetters.js
+++ countLetters.js
@@ -2,6 +2,7 @@ function countLetters(string) {
 	var count = {};
 	var foundLetters = [];
 
+	string = string.split(' ').join('');	
 	for (var i = 0; i < string.length; i++) {
 		foundLetters = Object.keys(count);
 		if (foundLetters.includes(string.charAt(i))) {
@@ -14,5 +15,5 @@ function countLetters(string) {
 	console.log(count);
 }
 
-countLetters('abcdefffggg');
+countLetters('abcde fffggg');
 countLetters('aaaaaabbbbbbbcccccccdef');
